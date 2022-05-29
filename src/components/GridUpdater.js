9dc87747b56ac06ff06da00b988c13af4a1afa5e
Lotoke <67965604+Lotoke@users.noreply.github.com>
2022-05-29 21:02:49 +0100

    commit

---
+++ src/components/GridUpdater.js
@@ -0,0 +1,30 @@
+import * as math from "mathjs";
+import Matrix from "./Matrix";
+import React from "react";
+import Box from "./Box";
+import { useState } from "react";
+import "../App";
+// Represents a 10 x 18 grid of grid squares
+
+export default function Grid(props) {
+  // generates an array of 18 rows, each containing 10 GridSquares.
+
+  const [grid, setGrid] = useState([]);
+  for (let row = 0; row < 10; row++) {
+    grid.push([]);
+    for (let col = 0; col < 5; col++) {
+      grid[row].push(
+        <Box
+          selectionStatus={"unselectedBox"}
+          key={`${col}${row}`}
+          index={`${col}${row}`}
+          val={Math.floor(Math.random() * (11 - 1) + 1)}
+        />
+      );
+    }
+  }
+
+  // The components generated in makeGrid are rendered in div.grid-board
+
+  return <div className="gridBoard">{grid}</div>;
+}
