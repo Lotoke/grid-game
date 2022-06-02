d9d5a73add9dde9bef269f5477f4a433e57de13d
Lotoke <67965604+Lotoke@users.noreply.github.com>
2022-06-02 22:58:16 +0100

    commit

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -8,8 +8,9 @@ import "../App";
 
 export default function Grid(props) {
   // generates an array of 18 rows, each containing 10 GridSquares.
-
+  const [boxStatus, setBoxStatus] = useState([]);
   const grid = [];
+
   for (let row = 0; row < 10; row++) {
     grid.push([]);
     for (let col = 0; col < 5; col++) {
@@ -19,12 +20,23 @@ export default function Grid(props) {
           key={`${col}${row}`}
           index={`${col}${row}`}
           val={Math.floor(Math.random() * (11 - 1) + 1)}
+          changeSelectionState={changeSelectionState}
         />
       );
     }
   }
-
+  setBoxStatus(grid);
+  const changeSelectionState = ()=> {
+    for (let row = 0; row < 10; row++) {
+      grid.push([]);
+      for (let col = 0; col < 5; col++) {
+        if ('${col}${row}' == index ){
+          //delete present box and replace with new box with 'selectedBox' status
+      }
+    }
+    setBoxStatus(grid);
+  }
   // The components generated in makeGrid are rendered in div.grid-board
-
+ 
   return <div className="gridBoard">{grid}</div>;
 }
