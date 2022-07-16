62c0cb20441f54a9d3eaa819cf878bca496e109d
BuildTools <unconfigured@null.spigotmc.org>
2022-07-16 16:44:14 +0100

    commit

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,18 +1,25 @@
+import { toBePartiallyChecked } from "@testing-library/jest-dom/dist/matchers";
 import * as math from "mathjs";
-
+import { speedOfLightDependencies } from "mathjs";
+import '../App.css';
 function Box() {
   //let box = document.createElement('div');
   
   return(
-<div style ={styles.box}></div>
+<div style ={styles.box} onClick={()=>alert("test")}></div>
   );
 }
 
 const styles = {
 box:{
-  backgroundColor: 'blue',
+  backgroundColor: '#fefbf0',
+  borderColor: '#d4d3d0',
   height:40,
-  width:40
+  width:40,
+  borderStyle: 'solid',
+  
+  borderWidth:'2px',
+
 }
 }
 
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Box from './Box'
-
+import '../App.css';
 // Represents a 10 x 18 grid of grid squares
 
 export default function Grid(props) {
@@ -8,9 +8,9 @@ export default function Grid(props) {
   // generates an array of 18 rows, each containing 10 GridSquares.
 
     const grid = []
-    for (let row = 0; row < 18; row ++) {
+    for (let row = 0; row < 10; row ++) {
         grid.push([])
-        for (let col = 0; col < 10; col ++) {
+        for (let col = 0; col < 6; col ++) {
             grid[row].push(<Box key={`${col}${row}`}/>)
         }
     }
@@ -18,17 +18,9 @@ export default function Grid(props) {
   // The components generated in makeGrid are rendered in div.grid-board
 
     return (
-        <div className='gridBoard'>
+        <div className='grid-board'>
             {grid}
         </div>
     )
 }
 
-const styles = {
-    gridBoard:{
-        display: grid,
-        gridTemplateColumns: 10px 17px,
-        gridGap: 0,
-        alignSelf: flexStart
-    }
-    }
